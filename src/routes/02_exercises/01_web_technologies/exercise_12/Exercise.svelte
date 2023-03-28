9754a51554bec8718a35197109cee0651287b90e
Kenbrown3 <brown.kendallanthony@gmail.com>
2023-03-28 16:39:01 -0700

    stuff

--- src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
@@ -1,10 +1,15 @@
 <script>
-	let clicked = false;
+  let clicked = false;
 </script>
 
 <div>
-	<button class="btn btn-primary" on:click={() => (clicked = true)}>
-		click here
-	</button>
-	<!--  -->
+  <button class="btn btn-primary" on:click={() => (clicked = true)}>
+    click here
+  </button>
+  <!--  -->
+  {#if !clicked}
+    You need to click the button
+  {:else}
+    You clicked the button
+  {/if}
 </div>
